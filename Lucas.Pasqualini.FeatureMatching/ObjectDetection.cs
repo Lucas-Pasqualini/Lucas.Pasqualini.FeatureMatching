934297d504ce542ea7f97c49d69e5576cf3f93d8
shootylife <a2x.lucas59@gmail.com>
2022-02-08 09:14:38 +0100

    feat: create unit test project & library

---
+++ Lucas.Pasqualini.FeatureMatching/ObjectDetection.cs
@@ -0,0 +1,10 @@
+﻿namespace Lucas.Pasqualini.FeatureMatching;
+
+public class ObjectDetection
+{
+    public IList<ObjectDetectionResult> DetectObjectInScenes(byte[]
+        objectImageData, IList<byte[]> imagesSceneData)
+    {
+        throw new NotImplementedException();
+    }
+}
